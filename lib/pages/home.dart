50dea310f82c9c2b1b27b8a0b1bc531483c3b54f
Elias Nassar <elias.nassar.22@gmail.com>
2024-01-31 16:41:53 +0200

    Minor UI changes to Homepage

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,23 +13,66 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-              child: const Text('Go to 100 Steps Page'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => StepCounter()), // Replace with your actual StatefulWidget
-                );
-              },
+            Container(
+              width: 100,
+              height: 100,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => StepCounter()),
+                  );
+                },
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.directions_walk),
+                    Flexible(
+                      fit: FlexFit.loose,
+                      child: Text(
+                        '500 Steps',
+                        style: TextStyle(fontSize: 10),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            ElevatedButton(
-              child: const Text('Go to Take Picture Page'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => TakePictureScreen(imagePaths: [],)), // Replace with your actual StatefulWidget
-                );
-              },
+            const SizedBox(height: 50,),
+            Container(
+              width: 100,
+              height: 100,
+              child: ElevatedButton(
+  style: ElevatedButton.styleFrom(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(20), // Adjust this value to your desired corner radius
+    ),
+  ),
+  onPressed: () {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => StepCounter()),
+    );
+  },
+  child: const Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Icon(Icons.camera_alt),
+      Flexible(
+        fit: FlexFit.loose,
+        child: Text(
+          'Trees',
+          style: TextStyle(fontSize: 10),
+        ),
+      ),
+    ],
+  ),
+),
             ),
           ],
         ),
