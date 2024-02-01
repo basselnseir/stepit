ca4a096979b8a1e67e6cf39b1659a2be6f9b957b
julianmour <julian.mour4@gmail.com>
2024-02-01 13:16:43 +0200

    up

--- lib/pages/100_steps.dart
+++ lib/pages/100_steps.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pedometer/pedometer.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/services.dart';
 
 
 // class MyApp extends StatelessWidget {
@@ -27,28 +29,50 @@ class _StepCounterState extends State<StepCounter> {
   late Pedometer _pedometer;
   late Stream<StepCount> _stepCountStream;
   String _steps = '0';
+  int _initialSteps = 0;
+  bool started = false;
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
+    _requestPermission();
+  }
+
+  void _requestPermission() async {
+    if (await Permission.activityRecognition.request().isGranted) {
+      // Either the permission was already granted before or the user just granted it.
+      // You can start listening to the pedometer here.
+    }
   }
 
   Future<void> initPlatformState() async {
     _pedometer = Pedometer();
     _stepCountStream = Pedometer.stepCountStream;
-    _stepCountStream.listen(onData)
-      ..onError(onError);
+    _stepCountStream.listen(onData).onError(onError);
+
+    // Get the initial step count
+    _stepCountStream.first.then((StepCount event) {
+      _initialSteps = event.steps;
+      started = true;
+    });
   }
 
   void onData(StepCount event) {
     setState(() {
-      _steps = event.steps.toString();
+      if (started){
+        _steps = (event.steps - _initialSteps).toString();
+      }
     });
   }
 
   void onError(error) {
     print('Flutter Pedometer Error: $error');
+    if (error is PlatformException && error.code == '1') {
+      setState(() {
+        _steps = 'Step counter not available on this device';
+      });
+    }
   }
 
   @override
