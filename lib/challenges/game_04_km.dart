1026daedb14a108280cd41128a79196568bd7d34
Elias Nassar <elias.nassar.22@gmail.com>
2024-06-19 09:45:46 +0300

    [KM] Add a message for user when leaving the challenge

--- lib/challenges/game_04_km.dart
+++ lib/challenges/game_04_km.dart
@@ -46,66 +46,98 @@ class _Game_04_kmState extends State<Game_04_km> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          children: <Widget>[
-            Image.asset(
-              Game.getGameIcon("3 km"), // Replace with your image path
-              width: 30, // Adjust the width as needed
-              height: 30, // Adjust the height as needed
+    return WillPopScope(
+            onWillPop: () async {
+        if (challengeStarted) {
+          return await showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: const Text('Warning'),
+                  content: const Text(
+                      'The challenge will reset if you leave. Are you sure you want to stop the challenge?'),
+                  actions: <Widget>[
+                    TextButton(
+                      child: Text('No'),
+                      onPressed: () {
+                        Navigator.of(context).pop(false);
+                      },
+                    ),
+                    TextButton(
+                      child: Text('Yes'),
+                      onPressed: () {
+                        distanceTraveled = 0;
+                        Navigator.of(context).pop(true);
+                      },
+                    ),
+                  ],
+                ),
+              ) ??
+              false;
+        } else {
+          return true;
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: <Widget>[
+              Image.asset(
+                Game.getGameIcon("3 km"), // Replace with your image path
+                width: 30, // Adjust the width as needed
+                height: 30, // Adjust the height as needed
+              ),
+              const SizedBox(width: 15), // Add some space between the title and the icon
+              const Text("3 km",
+                  style: TextStyle(
+                  fontSize: 20.0, // Adjust the font size as needed
+                  fontFamily: 'Roboto', // Change to your preferred font
+                  fontWeight: FontWeight.bold, // Make the text bold
+                )
+              ),
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                widget.description,
+                style: TextStyle(fontSize: 18),
+              ),
             ),
-            const SizedBox(width: 15), // Add some space between the title and the icon
-            const Text("3 km",
-                style: TextStyle(
-                fontSize: 20.0, // Adjust the font size as needed
-                fontFamily: 'Roboto', // Change to your preferred font
-                fontWeight: FontWeight.bold, // Make the text bold
-              )
+            Text(
+              'Distance Traveled: ${distanceTraveled.toStringAsFixed(2)} km',
+              style: TextStyle(fontSize: 24),
             ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: challengeStarted
+                  ? null
+                  : () {
+                      setState(() {
+                        challengeStarted = true;
+                      });
+                    },
+              child: Text('Start Challenge'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: challengeStarted ? Colors.grey : null,
+              ),
+            ),
+            // StreamBuilder<StepCount>(
+            //   stream: _stepCountStream,
+            //   builder: (context, snapshot) {
+            //     if (snapshot.hasData) {
+            //       // Here you can calculate the distance traveled based on the step count
+            //       // For simplicity, we're just showing the step count
+            //       return Text('Distance Traveled: ${(snapshot.data!.steps * 0.000762).toStringAsFixed(2)} km');
+            //     } else {
+            //       return Text('No step count data');
+            //     }
+            //   },
+            // ),
           ],
         ),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              widget.description,
-              style: TextStyle(fontSize: 18),
-            ),
-          ),
-          Text(
-            'Distance Traveled: ${distanceTraveled.toStringAsFixed(2)} km',
-            style: TextStyle(fontSize: 24),
-          ),
-          SizedBox(height: 16),
-          ElevatedButton(
-            onPressed: challengeStarted
-                ? null
-                : () {
-                    setState(() {
-                      challengeStarted = true;
-                    });
-                  },
-            child: Text('Start Challenge'),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: challengeStarted ? Colors.grey : null,
-            ),
-          ),
-          StreamBuilder<StepCount>(
-            stream: _stepCountStream,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                // Here you can calculate the distance traveled based on the step count
-                // For simplicity, we're just showing the step count
-                return Text('Distance Traveled: ${(snapshot.data!.steps * 0.000762).toStringAsFixed(2)} km');
-              } else {
-                return Text('No step count data');
-              }
-            },
-          ),
-        ],
       ),
     );
   }
